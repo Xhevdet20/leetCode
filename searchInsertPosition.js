13941263fe8dad663c7dfe416e0c2e49a7c06163
xhev-dev <devxhev@gmail.com>
2023-06-06 22:26:00 +0200

    search insert position

---
+++ searchInsertPosition.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let i;
+  for (i = 0; i < nums.length; i++) {
+    if (target <= nums[i]) {
+      return i;
+    }
+  }
+  return i;
+};
+
+searchInsert([1, 3, 5, 6], 7);
